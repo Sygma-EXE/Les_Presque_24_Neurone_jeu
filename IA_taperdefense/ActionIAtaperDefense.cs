5cf17d71287221c09e0b7cd1b3bd7d6ac824c991
mitthiax <matt.boisson21@gmail.com>
2025-05-17 17:45:06 +0200

    Connexion
    Elle se connecte

---
+++ IA_taperdefense/ActionIAtaperDefense.cs
@@ -0,0 +1,18 @@
+﻿using Interface_communication;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lejeu.IA_taperdefense
+{
+    public class ActionIAtaperDefense
+    {
+        Connexion Connexions = Connexion.Instance;
+        public ActionIAtaperDefense()
+        {
+            Console.WriteLine(Connexions.RecevoirMessage());
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Lejeu.IA_taperdefense;
+
+ActionIAtaperDefense actionIAtaperDefense = new ActionIAtaperDefense();
+
